6dc7d6632c9a499c2891ba6a870c04e68ec925ff
alisameta <163756707+alisameta09@users.noreply.github.com>
2025-08-29 11:37:26 +0300

    3.1 Inject RxJs methods to improve auth,

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {TokenResponse} from './auth.interface';
+import {tap} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +10,26 @@ export class AuthService {
   http = inject(HttpClient);
   baseApiUrl = 'https://icherniakov.ru/yt-course/auth/';
 
+  token: string | null = null;
+  refreshToken: string | null = null;
+
+  get isAuth() {
+    return !!this.token;
+  }
+
   login(payload: {username: string, password: string}) {
     const fd = new FormData();
 
     fd.append('username', payload.username);
     fd.append('password', payload.password);
 
-    return this.http.post(`${this.baseApiUrl}token`,
+    return this.http.post<TokenResponse>(`${this.baseApiUrl}token`,
       fd
+    ).pipe(
+      tap(val => {
+        this.token = val.access_token;
+        this.refreshToken = val.refresh_token;
+      })
     )
   }
 }
