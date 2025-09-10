92960d9e81261af90f64b7089b052c7df4b64399
alisameta <163756707+alisameta09@users.noreply.github.com>
2025-09-10 16:36:30 +0300

    1.1 Setting post-input component

--- src/app/pages/profile-page/post-input/post-input.component.ts
+++ src/app/pages/profile-page/post-input/post-input.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, inject, Renderer2} from '@angular/core';
+import {AvatarCircleComponent} from '../../../common-ui/avatar-circle/avatar-circle.component';
+import {ProfileService} from '../../../data/services/profile.service';
+import {SvgIconComponent} from '../../../common-ui/svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-post-input',
-  imports: [],
+  imports: [
+    AvatarCircleComponent,
+    SvgIconComponent
+  ],
   templateUrl: './post-input.component.html',
   styleUrl: './post-input.component.scss'
 })
 export class PostInputComponent {
+  r2 = inject(Renderer2);
+  profile = inject(ProfileService).me;
+
+  onTextAreaInput(event: Event) {
+    const textarea = event.target as HTMLTextAreaElement;
+
+    this.r2.setStyle(textarea, 'height', 'auto');
+    this.r2.setStyle(textarea, 'height', textarea.scrollHeight + 'px');
+  }
 
 }
