bbf51b7e9c95d3ec97529f6673219b8b93865bea
alisameta <163756707+alisameta09@users.noreply.github.com>
2025-09-12 09:14:00 +0300

    1.2 Creating post, service and interface

--- src/app/pages/profile-page/post-input/post-input.component.ts
+++ src/app/pages/profile-page/post-input/post-input.component.ts
@@ -2,25 +2,43 @@ import {Component, inject, Renderer2} from '@angular/core';
 import {AvatarCircleComponent} from '../../../common-ui/avatar-circle/avatar-circle.component';
 import {ProfileService} from '../../../data/services/profile.service';
 import {SvgIconComponent} from '../../../common-ui/svg-icon/svg-icon.component';
+import {PostService} from '../../../data/services/post.service';
+import {FormsModule} from '@angular/forms';
+import {firstValueFrom} from 'rxjs';
 
 @Component({
   selector: 'app-post-input',
   imports: [
     AvatarCircleComponent,
-    SvgIconComponent
+    SvgIconComponent,
+    FormsModule
   ],
   templateUrl: './post-input.component.html',
   styleUrl: './post-input.component.scss'
 })
 export class PostInputComponent {
   r2 = inject(Renderer2);
+  postService = inject(PostService);
   profile = inject(ProfileService).me;
+
+  postText = '';
 
   onTextAreaInput(event: Event) {
     const textarea = event.target as HTMLTextAreaElement;
 
     this.r2.setStyle(textarea, 'height', 'auto');
     this.r2.setStyle(textarea, 'height', textarea.scrollHeight + 'px');
+  }
+
+  onCreatePost() {
+    if (!this.postText) return;
+
+    firstValueFrom(this.postService.createPost({
+      title: 'Клевый пост',
+      content: this.postText,
+      authorId: this.profile()!.id,
+      communityId: 0
+    }))
   }
 
 }
