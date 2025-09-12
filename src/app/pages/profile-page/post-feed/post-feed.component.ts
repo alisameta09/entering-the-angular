6551b94d6ea194b3bd664cdcea834d420106c8d6
alisameta <163756707+alisameta09@users.noreply.github.com>
2025-09-12 17:39:29 +0300

    2.0 Setting scroll on post-feed component

--- src/app/pages/profile-page/post-feed/post-feed.component.ts
+++ src/app/pages/profile-page/post-feed/post-feed.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {Component, ElementRef, inject, Renderer2} from '@angular/core';
 import {PostInputComponent} from '../post-input/post-input.component';
 import {PostComponent} from '../post/post.component';
 import {PostService} from '../../../data/services/post.service';
@@ -14,11 +14,21 @@ import {firstValueFrom} from 'rxjs';
   styleUrl: './post-feed.component.scss'
 })
 export class PostFeedComponent {
+  r2 = inject(Renderer2)
   postService = inject(PostService);
+  hostElement = inject(ElementRef);
+
   feed = this.postService.posts;
 
   constructor() {
     firstValueFrom(this.postService.fetchPosts());
+  }
+
+  ngAfterViewInit() {
+    const {top} = this.hostElement.nativeElement.getBoundingClientRect();
+    const height = window.innerHeight - top - 24 - 24;
+
+    this.r2.setStyle(this.hostElement.nativeElement, 'height', `${height}px`);
   }
 
 }
