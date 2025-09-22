dc8924bc63d28f90966e535cbd854d05859d5a7f
alisameta <163756707+alisameta09@users.noreply.github.com>
2025-09-22 16:32:56 +0300

    2.2 Creating date pipe for posts and comments

---
+++ src/app/helpers/pipes/date-transform.pipe.ts
@@ -0,0 +1,36 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {DateTime} from 'luxon';
+
+@Pipe({
+  name: 'dateTransform'
+})
+export class DateTransformPipe implements PipeTransform {
+
+  transform(
+    value: string | null,
+    format: string = 'HH:mm dd.MM.yyyy',
+    relative: boolean = false,
+  ): string {
+
+    if (!value) return '';
+
+    const localizedDate = DateTime
+      .fromISO(value, {zone: 'utc'})
+      .setLocale('ru')
+      .toLocal();
+
+    if (relative) {
+      const diffInSeconds = Math.abs(localizedDate
+        .diffNow('seconds')
+        .seconds
+      );
+
+      if (diffInSeconds < 60) return 'только что';
+
+      return localizedDate.toRelative() ?? '';
+    }
+
+    return localizedDate.toFormat(format);
+  }
+
+}
