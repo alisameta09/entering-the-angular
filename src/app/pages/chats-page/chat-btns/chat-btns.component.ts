66542b5ba841a53df1a29719a074762b7c01b83f
alisameta <163756707+alisameta09@users.noreply.github.com>
2025-09-24 16:01:26 +0300

    1.1 Creating chats components and setting routing

---
+++ src/app/pages/chats-page/chat-btns/chat-btns.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-btns',
+  imports: [],
+  templateUrl: './chat-btns.component.html',
+  styleUrl: './chat-btns.component.scss'
+})
+export class ChatBtnsComponent {
+
+}
---
+++ src/app/pages/chats-page/chats-page.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-chats-page',
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './chats-page.component.html',
+  styleUrl: './chats-page.component.scss'
+})
+export class ChatsPageComponent {
+
+}
