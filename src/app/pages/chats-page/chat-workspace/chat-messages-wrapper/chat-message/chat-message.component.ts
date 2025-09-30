e33b810875fc47413df1b7a534ff569539367520
alisameta <163756707+alisameta09@users.noreply.github.com>
2025-09-30 09:57:54 +0300

    1.9 Chat layout, personalizing messages

--- src/app/pages/chats-page/chat-workspace/chat-messages-wrapper/chat-message/chat-message.component.ts
+++ src/app/pages/chats-page/chat-workspace/chat-messages-wrapper/chat-message/chat-message.component.ts
@@ -1,9 +1,14 @@
 import {Component, input} from '@angular/core';
 import {Message} from '../../../../../data/interfaces/chats.interface';
+import {AvatarCircleComponent} from '../../../../../common-ui/avatar-circle/avatar-circle.component';
+import {DateTransformPipe} from '../../../../../helpers/pipes/date-transform.pipe';
 
 @Component({
   selector: 'app-chat-message',
-  imports: [],
+  imports: [
+    AvatarCircleComponent,
+    DateTransformPipe
+  ],
   templateUrl: './chat-message.component.html',
   styleUrl: './chat-message.component.scss'
 })
