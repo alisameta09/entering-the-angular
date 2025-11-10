ecd47f94cab0b175cdaf02f429d3502e99e39ca6
alisameta <163756707+alisameta09@users.noreply.github.com>
2025-11-10 11:19:29 +0300

    1.2 Adding scrolling

--- libs/profile/src/lib/feature-profile-list/search-page/search-page.component.ts
+++ libs/profile/src/lib/feature-profile-list/search-page/search-page.component.ts
@@ -1,8 +1,18 @@
-import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  ElementRef,
+  inject,
+  Renderer2
+} from '@angular/core';
 import {ProfileCardComponent} from '../../ui/profile-card/profile-card.component';
 import {ProfileFiltersComponent} from '../profile-filters/profile-filters.component';
 import {selectFilteredProfiles} from '@tt/data-access/profile';
 import {Store} from '@ngrx/store';
+import {debounceTime, fromEvent} from 'rxjs';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-search-page',
@@ -11,7 +21,29 @@ import {Store} from '@ngrx/store';
   styleUrl: './search-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SearchPageComponent {
+export class SearchPageComponent implements AfterViewInit {
+  PADDING = 20 * 2
+
   store = inject(Store);
+  r2 = inject(Renderer2);
+  destroyRef = inject(DestroyRef);
+  hostElement = inject(ElementRef)
+
   profiles = this.store.selectSignal(selectFilteredProfiles);
+
+  ngAfterViewInit() {
+    this.resizeSearchPage();
+
+    fromEvent(window, 'resize')
+      .pipe(debounceTime(100), takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => this.resizeSearchPage());
+  }
+
+  resizeSearchPage() {
+    const el = this.hostElement.nativeElement;
+    const {top} = el.getBoundingClientRect();
+    const height = window.innerHeight - top - this.PADDING;
+
+    this.r2.setStyle(el, 'height', `${height}px`);
+  }
 }
